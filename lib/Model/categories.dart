f7a25c70a1ea7142cea3f6c104fb540742c3adcc
Yuri <yuri.strack@gmail.com>
2020-05-01 16:28:28 -0300

    Scratched listTiles for each procedure and prepared model to receive list of procedures

--- lib/Model/categories.dart
+++ lib/Model/categories.dart
@@ -1,8 +1,17 @@
+class Procedure {
+  final String image;
+  final String title;
+  final String description;
+
+  Procedure(this.image, this.title, this.description);
+}
+
 class Category {
   final String name;
   final String image;
+  // final List<Procedure> procedures;
 
-  Category(this.name, this.image);
+  Category(this.name, this.image /*, this.procedures*/);
 }
 
 List<Category> categories = categoriesData
@@ -13,3 +22,5 @@ var categoriesData = [
   {"name": "Lavar as mãos", 'image': "assets/images/wash-hands.png"},
   {"name": "Escovar os dentes", 'image': "assets/images/brush-teeth.png"},
 ];
+
+var proceduresData = [];
--- lib/activityScreen.dart
+++ lib/activityScreen.dart
@@ -17,49 +17,85 @@ class _ActivityScreenState extends State<ActivityScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          ClipPath(
-            clipper: MyClipper(),
-            child: Container(
-              padding: EdgeInsets.only(left: 20, top: 60, right: 40),
-              height: 300,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topRight,
-                      end: Alignment.bottomLeft,
-                      colors: [Color(0xFF3383CD), Color(0xFF11249F)])),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Align(
-                      alignment: Alignment.topLeft,
-                      child: GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (BuildContext context) => HomePage()));
-                          },
-                          child: SvgPicture.asset("assets/icons/menu.svg"))),
-                  SizedBox(height: 45),
-                  Expanded(
-                      child: Stack(children: <Widget>[
-                    Positioned(
-                        left: 0,
-                        child: Text("${widget.category.name}",
-                            style: kHeadingTextStyle.copyWith(
-                                color: Colors.white, fontFamily: "Poppins"))),
-                    Positioned(
-                        top: 40,
-                        child: Text("Aprenda mais sobre a atividade!",
-                            style: kLabelStyle.copyWith(
-                                color: Colors.white, fontSize: 16)))
-                  ]))
-                ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            ClipPath(
+              clipper: MyClipper(),
+              child: Container(
+                padding: EdgeInsets.only(left: 20, top: 60, right: 40),
+                height: 300,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.bottomLeft,
+                        colors: [Color(0xFF3383CD), Color(0xFF11249F)])),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Align(
+                        alignment: Alignment.topLeft,
+                        child: GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      HomePage()));
+                            },
+                            child: SvgPicture.asset("assets/icons/menu.svg"))),
+                    SizedBox(height: 45),
+                    Expanded(
+                        child: Stack(children: <Widget>[
+                      Positioned(
+                          left: 0,
+                          child: Text("${widget.category.name}",
+                              style: kHeadingTextStyle.copyWith(
+                                  color: Colors.white, fontFamily: "Poppins"))),
+                      Positioned(
+                          top: 40,
+                          child: Text("Aprenda mais sobre a atividade!",
+                              style: kLabelStyle.copyWith(
+                                  color: Colors.white, fontSize: 16)))
+                    ]))
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+            Padding(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Text(
+                          "Procedimentos",
+                          style: kCuidadosTitleTextstyle,
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Column(
+                      children: categories
+                          .map(
+                            (item) => Card(
+                              child: ListTile(
+                                leading: FlutterLogo(size: 56.0),
+                                title: Text('Nome do procedimento X',
+                                    style: kTitleTextstyle.copyWith(
+                                        fontFamily: 'Poppins', fontSize: 16)),
+                                subtitle: Text(
+                                  'Descricao de como fazer',
+                                  style: TextStyle(fontFamily: 'Poppins'),
+                                ),
+                              ),
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  ],
+                ))
+          ],
+        ),
       ),
     );
   }
